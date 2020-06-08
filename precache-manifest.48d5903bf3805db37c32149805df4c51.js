9a9252cd4c731f58ca80867435e8696c6b56c49a
lrjia <lrjia2017@gmail.com>
2020-06-09 00:16:47 +0800

    升级

---
+++ precache-manifest.48d5903bf3805db37c32149805df4c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cc41d76b465e66ee41467969fab3d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c2160fa4df3a5abc8e2",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "25cae1dbc1ea59aa1b19",
+    "url": "/static/js/2.5971239b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5971239b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2160fa4df3a5abc8e2",
+    "url": "/static/js/main.f4f60a01.chunk.js"
+  },
+  {
+    "revision": "41878c50e2e58b921fb5",
+    "url": "/static/js/runtime-main.9b7abc97.js"
+  }
+]);
